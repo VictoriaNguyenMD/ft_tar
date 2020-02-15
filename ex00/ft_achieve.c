68771773658bdd53e1dfa33e62f0e735efcb180d
Olorunfemi Durosinmi <oldurosi@e1z3r4p19.42.us.org>
2020-02-15 01:42:34 -0800

    Usage

--- ex00/ft_achieve.c
+++ ex00/ft_achieve.c
@@ -6,7 +6,7 @@
 /*   By: oldurosi <oldurosi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/15 01:20:02 by oldurosi          #+#    #+#             */
-/*   Updated: 2020/02/15 01:28:56 by oldurosi         ###   ########.fr       */
+/*   Updated: 2020/02/15 01:42:08 by oldurosi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,19 @@
 
 int main(int argc, char **argv)
 {
-    if (argc > 1)
+    FILE *tar;
+    if (argc > 2)
     {
-
+        tar = fopen(argv[1], "wb");
+        if( !tar )
+        {
+            fprintf( stderr, "Failed to open %s for writing\n", argv[1] );
+            return (-1);
+        }
     }
     else
     {
-        perror("Usage: ");
+        perror("Usage: file_archive [file1 ... fileN]\n");
     }
     
     return (0);
