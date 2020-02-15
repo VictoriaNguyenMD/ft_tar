b6ef26ebba20e89dd80d19bdf58f9b7658dd1aa6
Olorunfemi Durosinmi <oldurosi@e1z3r4p19.42.us.org>
2020-02-15 02:05:56 -0800

    My current point

--- ex00/ft_achieve.c
+++ ex00/ft_achieve.c
@@ -6,11 +6,49 @@
 /*   By: oldurosi <oldurosi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/15 01:20:02 by oldurosi          #+#    #+#             */
-/*   Updated: 2020/02/15 01:42:08 by oldurosi         ###   ########.fr       */
+/*   Updated: 2020/02/15 02:05:36 by oldurosi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_tar.h"
+
+int compress_file(FILE *tar, char *name)
+{
+    FILE *input;
+    size_t offset;
+    size_t index;
+
+    /*
+    ** Obtains the current value of the file pointer indicator  
+    */
+   
+    index = ftell(tar);
+    
+    
+
+}
+
+int ft_archive(FILE *tar, char  **name, int count)
+{
+    int i;
+    int ret;
+
+    ret = 0;
+    if (!tar)
+    {
+        return (-1);
+    }
+    i = 2;
+    while (i < count)
+    {
+        if (compress_file(tar, name[i]))
+        {
+            break ;
+        }
+        ++i;
+    }
+    return;
+}
 
 int main(int argc, char **argv)
 {
@@ -18,7 +56,7 @@ int main(int argc, char **argv)
     if (argc > 2)
     {
         tar = fopen(argv[1], "wb");
-        if( !tar )
+        if(ft_archive(tar, argv, argc) != 0)
         {
             fprintf( stderr, "Failed to open %s for writing\n", argv[1] );
             return (-1);
--- ex00/ft_unarchive.c
+++ ex00/ft_unarchive.c
@@ -6,13 +6,27 @@
 /*   By: oldurosi <oldurosi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/15 01:29:39 by oldurosi          #+#    #+#             */
-/*   Updated: 2020/02/15 01:30:10 by oldurosi         ###   ########.fr       */
+/*   Updated: 2020/02/15 02:05:34 by oldurosi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_tar.h"
 
-int main()
+int main(int argc, char **argv)
 {
+    FILE *tar;
+    if (argc == 2)
+    {
+        tar = fopen(argv[1], "wb");
+        if(ft_unarchive(tar, argv, argc) != 0)
+        {
+            fprintf( stderr, "Failed to open %s for writing\n", argv[1] );
+            return (-1);
+        }
+    }
+    else
+    {
+        perror("Usage: file_archive\n");
+    }
     return (0);
 }
