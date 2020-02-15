bf18111945b61158d5f7c9bd7529dcd86219b30a
Olorunfemi Durosinmi <oldurosi@e1z3r4p19.42.us.org>
2020-02-15 01:30:26 -0800

    Getting all started

--- ex00/ft_tar.h
+++ ex00/ft_tar.h
@@ -6,7 +6,7 @@
 /*   By: oldurosi <oldurosi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/15 00:51:48 by oldurosi          #+#    #+#             */
-/*   Updated: 2020/02/15 00:55:00 by oldurosi         ###   ########.fr       */
+/*   Updated: 2020/02/15 01:27:06 by oldurosi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
-struct mdata
+// #include <exit.h>
+// struct mdata
+// {
+//    char name[255];
+//    FILE *fp;
+//    int size;
+//    int nsize;
+//    int ssize;
+//    struct mdata *next;
+// };
+
+typedef struct		s_mdata
 {
-   char name[255];
-   FILE *fp;
-   int size;
-   int nsize;
-   int ssize;
-   struct mdata *next;
-};
+	char			name[100];
+	char			mode[8];
+	char			owner[8];
+	char			group[8];
+	char			size[12];
+	char			modified[12];
+	char			checksum[8];
+	char			type[1];
+	char			link[100];
+	char			padding[255];
+	struct 			mdata *next;
+}					t_mdata;
 
 #endif
